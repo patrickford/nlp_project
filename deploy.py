f5dbd9d22c953d242e5e1369b3a54633f4f1b7c9
Yuri Yerastov <yuriyerastov@Yuris-MacBook-Pro.local>
2017-04-17 12:26:04 -0500

    Revised commit

--- deploy.py
+++ deploy.py
@@ -3,10 +3,11 @@ from subprocess import call
 import test
 
 def update_remote():
-    command_add = "git add ."
+    add = '''git add .'''
     commit_revised = '''git commit -m "Revised commit"'''
     push_github = '''git push origin master'''
     push_heroku = '''git push heroku master'''
+    call([add], shell=True)
     call([commit_revised], shell=True)
     call([push_github], shell=True)
     call([push_heroku], shell=True)
