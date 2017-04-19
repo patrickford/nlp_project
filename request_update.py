46a204d42d8b9cf92721e87ccf8a80e280764c3b
Yuri Yerastov <yuriyerastov@Yuris-MacBook-Pro.local>
2017-04-19 12:15:10 -0500

    cleanup2

--- request_update.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-payload = {'username': 'yurik4', 'first': 'Yuri', 'last':'Yerastoff', 'email':'y_yerastov@fhsu.edu'}
-r = requests.put("http://localhost:8080/user", data=payload)
-print r.content
